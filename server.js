d8e58e0f2b0176f0b9f997d716b8af3e0e0c96b1
Eirik Brandtzæg <eirikb@eirikb.no>
2011-02-09 22:27:46 +0100

    Init via nodester-cli

--- server.js
+++ server.js
@@ -1,28 +1,5 @@
-var http = require('http'),
-url = require('url'),
-nodeStatic = require('node-static');
-
-http.createServer(function(req, res) {
-	var publicFiles = new nodeStatic.Server('public', {
-		cache: false
-	});
-	var location = url.parse(req.url, true),
-	params = (location.query || req.headers);
-
-	switch (location.pathname) {
-	case '/eval':
-		res.writeHead(200, {
-			'Content-Type': 'text/plain'
-		});
-		var q = url.parse(req.url, true).query;
-		try {
-			res.end('' + eval(q.query));
-		} catch(e) {
-			res.end('' + e);
-		}
-	default:
-		publicFiles.serve(req, res);
-		break;
-	}
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
 }).listen(8491);
-
